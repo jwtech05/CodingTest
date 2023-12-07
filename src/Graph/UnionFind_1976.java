5a2d85d9d61f6c046cf838e4e7867749ecd57a34
jwAhn1020 <takartin@gmail.com>
2023-12-07 18:52:25 +0900

    12/07 refactor : 백준 1043번 테스트 케이스 통과 완료

--- src/Graph/UnionFind_1976.java
+++ src/Graph/UnionFind_1976.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class UnionFInd_1976 {
+public class UnionFind_1976 {
     static ArrayList<ArrayList<Integer>> road;
     static int[] unionFind;
     public static void union(int a, int b){
---
+++ src/TestPrepare/bufferTest.java
@@ -0,0 +1,19 @@
+package TestPrepare;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class bufferTest {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        String b = br.readLine();
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
