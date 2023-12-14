9697385f798e2daa81849220975ca506f091581b
jwAhn1020 <takartin@naver.com>
2023-12-14 20:02:30 +0900

    12/14 refactor : 백준 11404번 플로이드 와샬 한번 더 이해해보기

--- src/Graph/FloydWarshall_11404.java
+++ src/Graph/FloydWarshall_11404.java
@@ -8,64 +8,44 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class FloydWarshall_11404 {
-    static final int INF = 987654321;
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
+        StringBuilder sb;
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N + 1][N + 1];
-
+        int[][] distance = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                arr[i][j] = INF;
-
-                if (i == j) {
-                    arr[i][j] = 0;
-                }
+                if(i==j) distance[i][j] = 0;
+                else distance[i][j] = 10000001;
             }
         }
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            arr[a][b] = Math.min(arr[a][b], c);
+        for(int i=0; i<M; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            if(distance[s][e] > v) distance[s][e] = v;
         }
-
-
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
-                        arr[i][j] = arr[i][k] + arr[k][j];
+        for(int k=1; k<=N; k++){
+            for(int i=1; i<=N; i++){
+                for(int j=1; j<=N; j++){
+                    if(distance[i][j] > distance[i][k] + distance[k][j]){
+                        distance[i][j] = distance[i][k] + distance[k][j];
                     }
                 }
             }
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (arr[i][j] == INF) {
-                    arr[i][j] = 0;
-                }
-
-                sb.append(arr[i][j] + " ");
+        sb = new StringBuilder();
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                if(distance[i][j] == 10000001)
+                    distance[i][j] = 0;
+                sb.append(distance[i][j]+ " ");
             }
             sb.append("\n");
         }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
-
 }
  
