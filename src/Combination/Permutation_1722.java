6be0d317235db513a2a606f7becd4a63daa249f8
jwAhn1020 <takartin@naver.com>
2023-12-23 18:58:59 +0900

    12/23 feat : 백준 1256번 조합 구현 완료 - 점화식과 배열을 좀 더 잘 엮을 수 있기

--- src/Combination/Permutation_1722.java
+++ src/Combination/Permutation_1722.java
@@ -3,10 +3,9 @@ package Combination;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Combination_1722 {
+public class Permutation_1722 {
     public static void main(String[] args) throws IOException {
         long[] F = new long[21];
         int[] S = new int[21];
