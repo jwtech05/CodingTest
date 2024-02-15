f5ef9f20afa0d60d5b37eaf54b353474588fd69e
jwtech05 <takartin@gmail.com>
2024-02-15 19:18:51 +0900

    02/15 : 소프티어 - 로드밸런서 트래픽 예측 문제 클리어

--- src/소프티어/로드_밸런서_트래픽_예측.java
+++ src/소프티어/로드_밸런서_트래픽_예측.java
@@ -7,14 +7,22 @@ public class 로드_밸런서_트래픽_예측 {
     static ArrayList<ArrayList<Integer>> arr;
     static int[] arrBig;
     static long[] arrCnt;
+    static int[] arrBfs;
+    static boolean[] arrFlag;
+    static ArrayList<Integer> ordering;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         long K = Long.parseLong(st.nextToken());
         arr = new ArrayList<>();
+        ordering = new ArrayList<>();
+        ordering.add(0);
         arrBig = new int[N+1];
         arrCnt = new long[N+1];
+        arrBfs = new int[N+1];
+        arrFlag = new boolean[N+1];
+        arrFlag[1] = true;
         for(int i=0; i<=N; i++){
             arr.add(new ArrayList<>());
         }
@@ -25,21 +33,52 @@ public class 로드_밸런서_트래픽_예측 {
             while(st.hasMoreTokens()){
                 int next = Integer.parseInt(st.nextToken());
                 arr.get(i).add(next);
+                arrBfs[next]++;
             }
         }
-        for(long i = 0; i< K; i++){
-            DFS(1, arrCnt[1]);
+        BFS(1);
+        long[] answer = new long[N+1];
+        answer[1] = K;
+        for(int i=1; i < answer.length; i++){
+            int node = ordering.get(i);
+            long request = answer[node];
+            if(arr.get(node).size() == 0) continue;
+            long ans = request / arr.get(node).size();
+            long left = request % arr.get(node).size();
+            for(int j=0; j<arr.get(node).size(); j++){
+                answer[arr.get(node).get(j)] += ans;
+            }
+            for(int j=0; j<left; j++){
+                answer[arr.get(node).get(j)] += 1;
+            }
         }
-        for(int i=1; i<=N; i++){
-            System.out.print(arrCnt[i]+" ");
+        for(int i=1; i<answer.length; i++){
+            System.out.print(answer[i]);
+            if(i != answer.length-1) System.out.print(" ");
         }
     }
-    public static void DFS(int num, long cnt){
+/*    public static void DFS(int num, long cnt){
         arrCnt[num]++;
         if(arrBig[num] == 0){
             return;
         }
         int nextNum = (int) cnt % arrBig[num];
         DFS(arr.get(num).get(nextNum), arrCnt[arr.get(num).get(nextNum)]);
+    }*/
+
+    public static void BFS(int num) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(num);
+        while(!q.isEmpty()){
+            int next = q.poll();
+            ordering.add(next);
+            for(int x : arr.get(next)){
+                arrBfs[x]--;
+                if(arrBfs[x] == 0 && !arrFlag[x]){
+                    q.offer(x);
+                    arrFlag[x] = true;
+                }
+            }
+        }
     }
 }
