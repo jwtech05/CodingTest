3df14522b4af1c23fad644e10f828ef630487970
jwtech05 <takartin@gmail.com>
2024-03-05 23:20:33 +0900

    03/05 : 정렬 관련 문제 풀이 6차 실패

--- src/백준/Sorting/Sorting_5052.java
+++ src/백준/Sorting/Sorting_5052.java
@@ -8,21 +8,21 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /*
-* 1차 시도 : 실패
-* 소요 시간 : 2시간 5분
+* 6차 시도 : 실패
+* 소요 시간 : 3시간 22분
 * 실패 원인 :
 *
 * */
 public class Sorting_5052 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int tc = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int[][] arr;
         int[][] checkArr;
         for(int i = 0; i<tc; i++){
             String answer = "YES";
-            arr = new int[10001][10];
+            arr = new int[10000][10];
             checkArr = new int[10][10];
             for (int j = 0; j < arr.length; j++) {
                 Arrays.fill(arr[j], -1);
@@ -30,7 +30,7 @@ public class Sorting_5052 {
             int ec = Integer.parseInt(br.readLine());
             for(int j=0; j<ec; j++){
                 String num = br.readLine();
-                for(int k=0; k< num.length(); k++){
+                for(int k=0; k < num.length(); k++){
                     arr[j][k] = num.charAt(k)-'0';
                 }
             }
@@ -41,19 +41,19 @@ public class Sorting_5052 {
             }
             for(int j=0; j<ec; j++){
                 boolean flag = false;
-                for(int k=0; k<arr[ec].length; k++){
+                for(int k=0; k<arr[j].length; k++){
                     if(arr[j][k] == -1) continue;
                     //하나라도 1이 있으면 일관성이 있는 것
-                    if(checkArr[k][arr[j][k]] == 1){
-                        flag = true;
-                    }
+                    if(checkArr[k][arr[j][k]] == 1) flag = true;
                 }
                 if(!flag){
                     answer = "NO";
                     break;
                 }
             }
-            System.out.println(answer);
+            sb.append(answer);
+            if(i != tc-1) sb.append("\n");
         }
+        System.out.println(sb.toString());
     }
 }
