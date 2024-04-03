bac0d4c71ee7489bc6c9448c91fc38b3a4f93004
jwtech05 <takartin@gmail.com>
2024-04-03 14:02:12 +0900

    04/02 : 2212 문제 풀이 성공

--- src/백준/fiveJuCha/BackJun_2437.java
+++ src/백준/fiveJuCha/BackJun_2437.java
@@ -3,14 +3,9 @@ package 백준.fiveJuCha;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-/*
- * 시도 : 1회
- * 시간 : 1시간
- * 성공 여부 : 실패
- * 시간초과
- * */
 public class BackJun_2437 {
     static int N;
     static int[] arr;
@@ -22,29 +17,18 @@ public class BackJun_2437 {
         N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         arr = new int[N];
+        stop = new boolean[N];
+        flag = new boolean[1000000000];
+        flag[0] = true;
+        pq = new PriorityQueue<>();
         for(int i=0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        Arrays.sort(arr);
-        int num = arr[0];
-        int start = 1;
-        int end = 0;
-        for(int i=1; i<1000000; i++){
-            boolean check = false;
-            while(start < N){
-                if(i > num){
-                    num += arr[start];
-                    start++;
-                }else if(i < num){
-                    num -= arr[end];
-                    end++;
-                }else{
-                    check = true;
-                    break;
-                }
-            }
-            if(!check){
+        DFS(0, 0);
+        int size = pq.size();
+        for(int i = 1; i<size+1; i++){
+            int a = pq.poll();
+            if(i != a) {
                 answer = i;
                 break;
             }
@@ -52,4 +36,20 @@ public class BackJun_2437 {
         System.out.println(answer);
     }
 
+    public static void DFS(int start, int sum){
+        if(start == N){
+            return;
+        }
+        for(int i=0; i<N; i++){
+            if(stop[i]) continue;
+            if(!flag[sum]) {
+                pq.offer(sum);
+                flag[sum] = true;
+            }
+            stop[i] = true;
+            DFS(i,sum+arr[i]);
+            stop[i] = false;
+        }
+    }
 }
+
